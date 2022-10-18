855af95331233ed00bb7082645a21cc44bcada94
muhammadkhanusmanov <uqaxromon@gmail.com>
2022-10-18 15:54:27 +0500

    9-masala

--- if09.py
+++ if09.py
@@ -9,4 +9,11 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    t=a%10
+    o=(a%100-t)//10
+    new_num=t*10+o
+    if a>=new_num:
+        b = True
+    else:
+        b = False
+    return b 
